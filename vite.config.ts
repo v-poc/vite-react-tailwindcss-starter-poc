2b71c3e03a592febfb1c76503ff9e29eb31dd325
NikoNi <bluepspower@163.com>
2026-04-15 00:03:09 +0800

    refactor: switch to vite-plus

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite-plus";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  fmt: {},
+  lint: {"options":{"typeAware":true,"typeCheck":true}},
   plugins: [react()],
-})
+});
